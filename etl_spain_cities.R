676fdd95a8a9cc0e83062d9ba0ff7509671d5ff9
laudf <ladelafu@ucm.es>
2019-03-24 15:07:25 +0100

    modify script to download the location of spanish cities

--- etl_spain_cities.R
+++ etl_spain_cities.R
@@ -1,3 +1,4 @@
+
 # This file builds a dataframe with Spain cities and its attrs.
 # Finally, this dataframe is saved as a csv file.
 #
@@ -7,7 +8,7 @@
 
 rm(list=ls())
 
-webSource <- "https://es.wikipedia.org/wiki/Anexo:Municipios_de_EspaÃ±a_por_poblaciÃ³n"
+webSource <- "https://es.wikipedia.org/wiki/Anexo:Municipios_de_España_por_población"
 
 if (!require("devtools")) install.packages("devtools")
 require(devtools)
@@ -33,8 +34,8 @@ if(url.exists(webSource)){
   headerNames <- c("#", "Name", "Population", "Province", "AutonomousCommunity")
   
   # Let's create an empty dataframe where store all the tables we want
-  df <- data.frame(matrix(ncol = 4, nrow = 0))
-  colnames(df) <- c(headerNames[2], headerNames[3], headerNames[4], headerNames[5])
+  df <- data.frame(matrix(ncol = 7, nrow = 0))
+  colnames(df) <- c(headerNames[2], headerNames[3], headerNames[4], headerNames[5], "Urls")
   
   tryAsInteger = function(node) {
     val = xmlValue(node)
@@ -44,16 +45,32 @@ if(url.exists(webSource)){
     else
       ans
   }
-
+  
+  getUrl = function(node){
+    node = node[[1]]
+    size = length(node[[1]])
+    href = xmlGetAttr(node, "href")
+    while(size == 1){
+      href = xmlGetAttr(node, "href")
+      node = node[[1]]
+      size = length(node[[1]])
+     
+    }
+   href
+  }
+  
   for (i in tableIndex){
     table <- readHTMLTable(cityTables[[i]], header = headerNames, skip.rows = 1,
                            elFun = tryAsInteger)
+    tableUrls <- readHTMLTable(cityTables[[i]], header = headerNames, skip.rows = 1,
+                            elFun = getUrl)
     dff <- data.frame(
       Name = as.vector(table$Name),
       Population = as.vector(table$Population),
       Province = as.vector(table$Province),
       # TODO there's a bug which duplicates this record
-      AutonomousCommunity = as.vector(table$AutonomousCommunity)
+      AutonomousCommunity = as.vector(table$AutonomousCommunity),
+      Urls = as.vector(tableUrls$Name)
     )
     
     df <- rbind(df, dff)
@@ -63,9 +80,36 @@ if(url.exists(webSource)){
   # TODO Append as cols the geo location in the df
   #
   ##################################################
+  cityUrl = as.vector(df[, "Urls"])
+  
+  dfCoordinates <- data.frame(matrix(ncol = 2, nrow = 0))
+  colnames(dfCoordinates) <- c("Latitude", "Longitude")
+  
+
+  
+  for (i in 1:length(cityUrl)){
+    webPageCity <-  paste("https://es.wikipedia.org", cityUrl[i], sep = "/")
+    if(url.exists(webPageCity)) 
+    {
+      theWebPage <- getURL(webPageCity, ssl.verifypeer = FALSE)
+      theParsedWebPage <- htmlParse(theWebPage)
+      coordinates <- getNodeSet(theParsedWebPage, "//span[@id='coordinates']/span/a/span[@class='geo-nondefault']/span/span/span")
+      free(theParsedWebPage)
+      latitude = xmlValue(coordinates[[1]])
+      longitude = xmlValue(coordinates[[2]])
+      latitude = gsub(", $", "", latitude)
+      coordinates <- data.frame(
+        Latitude = as.vector(latitude),
+        Longitude = as.vector(longitude)
+      )
+      dfCoordinates <- rbind(dfCoordinates, coordinates)
+    }
+  }
+  df <- cbind(df, dfCoordinates)
+  
   
   # Store the df as csv
-  write.table(df, row.names = FALSE, file = "datawarehouse/cities.csv", sep = ";",
+  write.table(df, row.names = FALSE, append = TRUE, file = "datawarehouse/cities.csv", sep = ";",
               fileEncoding="UTF-8")
   
 } else {

4a01d59ac583cf8952dbb4b8eaefc48a15786025
FRYoussef <f.r.youssef@hotmail.com>
2019-04-28 21:05:32 +0200

    Removed key

--- twitter_scripts/twitter_conexion.R
+++ twitter_scripts/twitter_conexion.R
@@ -12,12 +12,12 @@ if (!is.installed("twitteR")){
 library(twitteR)
 
 
-consumerKey <- "vFZ7CB6wTOpZb8HmKacX5rgrE"
+consumerKey <- ""
 
-consumerSecret <- "cJapZyZfEu4C2uUhhZnWYO1RyAYpsqDuqknz9DjQbabV0f73Pm"
+consumerSecret <- ""
 
-accessToken <- "1097204050679001090-Pm2XtSrI6tpCLQrJvkAYKfdyvJfRgn"
+accessToken <- ""
 
-accessSecret <- "FrHQq4nbc4Rk9Y9Axjl6PhlIdHPxvNX5Gr0gF9dhBYTrU"
+accessSecret <- ""
 
 setup_twitter_oauth(consumerKey, consumerSecret, accessToken, accessSecret)

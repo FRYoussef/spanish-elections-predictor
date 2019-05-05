6ffd8184be876743349b437219473b1d8009659b
laudf <ladelafu@ucm.es>
2019-05-06 00:00:08 +0200

    Finished the words count and modify the clean tweets

--- clean_data/cleanTweets.R
+++ clean_data/cleanTweets.R
@@ -9,49 +9,42 @@ require(tm)
 getwd()
 setwd("C:/Users/laufu/Documents/Cuarto/Segundo cuatri/MIN/Proyecto/ElectionsPredictor-Complex_Networks")
 
-tweets <- read.csv("datawarehouse/tweets_2019-04-19.csv", sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
-nTweets<-length(tweets$Tweets)
-for(i in 1:nTweets){tweets[i,]$Woeid <- strtoi(tweets[i,]$Woeid, base = 0L)}
-tweets <- tweets[!is.na(tweets$Woeid),]
-nTweets<-length(tweets$Tweets)
-myCorpus<-Corpus(VectorSource(tweets$Tweets))
-#remove puntuation
-#myCorpus1<-tm_map(myCorpus,removePunctuation,ucp=TRUE)
-
-#remove capitals
-#for(i in 1:nTweets){myCorpus[[i]]$content<-tolower(myCorpus[[i]]$content)}
-myCorpus <- tm_map(myCorpus, tolower)
-#function that remove accents
-removeAccents <- content_transformer(function(x) chartr("αινσϊ", "aeiou", x))
-myCorpus <- tm_map(myCorpus, removeAccents)
-#function that remove URLs
-removeURL<-function(x)gsub("http(s)?://[[:alnum:]]*(.[[:alnum:]]*)*(/[[:alnum:]]*)*","",x) 
-myCorpus<-tm_map(myCorpus,removeURL)        
-myCorpus<-tm_map(myCorpus,removePunctuation,ucp=TRUE)
-#remove words without a lexical load
-myStopwords<-stopwords('spanish')
-myCorpus<-tm_map(myCorpus,removeWords,myStopwords)
-#eliminar caracteres raros
-removeCRT<-function(x)gsub("(<[[:alnum:]]*([[:punct:]][[:alnum:]]*)?>)*","",x)
-myCorpus<-tm_map(myCorpus,removeCRT) 
-removeRt<-function(x)gsub("rt","",x)
-myCorpus<-tm_map(myCorpus,removeRt) 
-myCorpus <-tm_map(myCorpus,removeNumbers)
-myCorpus <- tm_map(myCorpus, stripWhitespace)
-
-m=data.frame(text = sapply(myCorpus, as.character), stringsAsFactors = FALSE)
-tweets <- tweets[,3]
-tweets<-cbind(tweets, m)
-write.table(tweets, row.names = FALSE, file = "datawarehouse/clean_tweets_2019-04-19.csv", sep = ";", fileEncoding = "UTF-8", append = FALSE)
-
-
-
-
-
-#load the words list
-words <- read.csv("datawarehouse/wordsList_v2.csv", sep = ";", fileEncoding = "windows-1252", header=TRUE)
-print(tweets[2,])
-#filter into dataframe
-r <- grep("espana", words$Palabra, ignore.case=TRUE)
-print(r)
-words1 <- words[r,]
+for (day in 19:28){
+  name1 <- paste(day, "csv", sep=".")
+  name <- paste("datawarehouse/tweets_2019-04", name1, sep="-")
+  tweets <- read.csv(name, sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
+  nTweets<-length(tweets$Tweets)
+  for(i in 1:nTweets){tweets[i,]$Woeid <- strtoi(tweets[i,]$Woeid, base = 0L)}
+  tweets <- tweets[!is.na(tweets$Woeid),]
+  nTweets<-length(tweets$Tweets)
+  myCorpus<-Corpus(VectorSource(tweets$Tweets))
+  #remove puntuation
+  #myCorpus1<-tm_map(myCorpus,removePunctuation,ucp=TRUE)
+  
+  #remove capitals
+  #for(i in 1:nTweets){myCorpus[[i]]$content<-tolower(myCorpus[[i]]$content)}
+  myCorpus <- tm_map(myCorpus, tolower)
+  #function that remove accents
+  removeAccents <- content_transformer(function(x) chartr("αινσϊ", "aeiou", x))
+  myCorpus <- tm_map(myCorpus, removeAccents)
+  #function that remove URLs
+  removeURL<-function(x)gsub("http(s)?://[[:alnum:]]*(.[[:alnum:]]*)*(/[[:alnum:]]*)*","",x) 
+  myCorpus<-tm_map(myCorpus,removeURL)        
+  myCorpus<-tm_map(myCorpus,removePunctuation,ucp=TRUE)
+  #remove words without a lexical load
+  myStopwords<-stopwords('spanish')
+  myCorpus<-tm_map(myCorpus,removeWords,myStopwords)
+  #eliminar caracteres raros
+  removeCRT<-function(x)gsub("(<[[:alnum:]]*([[:punct:]][[:alnum:]]*)?>)*","",x)
+  myCorpus<-tm_map(myCorpus,removeCRT) 
+  removeRt<-function(x)gsub("rt","",x)
+  myCorpus<-tm_map(myCorpus,removeRt) 
+  myCorpus <-tm_map(myCorpus,removeNumbers)
+  myCorpus <- tm_map(myCorpus, stripWhitespace)
+  
+  m=data.frame(text = sapply(myCorpus, as.character), stringsAsFactors = FALSE)
+  tweets <- tweets[,"Woeid"]
+  tweets<-cbind(tweets, m)
+  name <- paste("datawarehouse/clean_tweets_2019-04", name1, sep="-")
+  write.table(tweets, row.names = FALSE, file = name, sep = ";", fileEncoding = "UTF-8", append = FALSE)
+}
--- clean_data/countWords.R
+++ clean_data/countWords.R
@@ -9,18 +9,68 @@ require(tm)
 getwd()
 setwd("C:/Users/laufu/Documents/Cuarto/Segundo cuatri/MIN/Proyecto/ElectionsPredictor-Complex_Networks")
 
-clean_tweets <- read.csv("datawarehouse/clean_tweets_2019-04-19.csv", sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
+
 cities <- read.csv("datawarehouse/cities.csv", sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
 #load the words list
 words <- read.csv("datawarehouse/wordsList_v2.csv", sep = ";", fileEncoding = "windows-1252", header=TRUE)
-for(i in 1:1){
-  woeid <- cities[i,]$Woeid
-  citie <- cities[i,]$Name
-  tweets_citie <- subset(clean_tweets, tweets==woeid)
-  myCorpus<-Corpus(VectorSource(tweets_citie$text))
-  terms <- TermDocumentMatrix(myCorpus)
-  matrix_terms = as.matrix(terms)
-  matrix_sort <- sort(rowSums(matrix_terms),decreasing=TRUE)
-  frec_terms <- data.frame(word = names(matrix_sort), freq=matrix_sort)
+def <- data.frame(matrix(ncol = 6, nrow = 0))
+colnames(def) <- c("City", "Support_PP", "Support_PSOE", "Support_Cs", "Support_Podemos", "Support_VOX")
+for (day in 19:28){
+  name1 <- paste(day, "csv", sep=".")
+  name <- paste("datawarehouse/clean_tweets_2019-04", name1, sep="-")
+  clean_tweets <- read.csv(name, sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
+  df <- data.frame(matrix(ncol = 6, nrow = 0))
+  colnames(df) <- c("City", "Support_PP", "Support_PSOE", "Support_Cs", "Support_Podemos", "Support_VOX")
+  #count words for each city.
+  for(i in 1:10){
+    woeid <- cities[i,]$Woeid
+    citie <- cities[i,]$Name
+    #get the data of the same woeid
+    tweets_citie <- subset(clean_tweets, tweets==woeid)
+    #form a corpus with the previous data(the same woeid).
+    myCorpus<-Corpus(VectorSource(tweets_citie$text))
+    #get the terms and their frequency
+    terms <- TermDocumentMatrix(myCorpus)
+    matrix_terms = as.matrix(terms)
+    matrix_sort <- sort(rowSums(matrix_terms),decreasing=TRUE)
+    frec_terms <- data.frame(word = names(matrix_sort), freq=matrix_sort)
+    dff <- data.frame(
+      City = citie,
+      Support_PP = 0,
+      Support_PSOE = 0,
+      Support_Cs = 0,
+      Support_Podemos = 0,
+      Support_VOX = 0
+    )
+    #initialize definitive data frame
+    if(day == 19){
+      def <- rbind(def, dff)
+    }
+    #search for each word in the "wordList" in the dataframe of frec terms
+    for (j in 1:nrow(words)){
+      r <- match(words[j,1], frec_terms$word)
+      frec <- frec_terms[r,2]
+      if (is.na(frec)){
+        for(k in 2:6){
+          dff[,k] = (0 * words[j,k])+dff[,k]
+        }
+      
+      }
+      else{
+        for(k in 2:6){
+          dff[,k] = words[j,k]+dff[,k]
+        }
+      }
+    }
+
+    df <- rbind(df, dff)
+  }
+  #update the definitive dataframe with the data for each day
+  for(k in 2:6){
+    def[,k] = def[,k]+df[,k]
+  }
+
 }
+write.table(def, row.names = FALSE, append = FALSE, file = "datawarehouse/count_list.csv", sep = ";",
+            fileEncoding="UTF-8")
 

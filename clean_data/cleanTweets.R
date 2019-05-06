51175058c845c8e19879ce3628f3799ff0a87bc5
FRYoussef <f.r.youssef@hotmail.com>
2019-05-06 19:38:17 +0200

    Code cleaning, and added a count list per day

--- clean_data/cleanTweets.R
+++ clean_data/cleanTweets.R
@@ -2,38 +2,48 @@
 
 rm(list=ls())
 
-
-if (!is.installed("tm")){
+if (!"tm" %in% rownames(installed.packages())){
   install.packages("tm")
 }
+
 library(tm)
 require(tm)
 
-for (day in 19:28){
-  name1 <- paste(day, "csv", sep=".")
-  name <- paste("datawarehouse/raw/tweets/tweets_2019-04", name1, sep="-")
-  tweets <- read.csv(name, sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
+files <- list.files(path="datawarehouse/raw/tweets", pattern="*.csv", full.names=TRUE, recursive=FALSE)
+
+for (file in files){
+  
+  tweets <- read.csv(file, sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
   nTweets<-length(tweets$Tweets)
-  for(i in 1:nTweets){tweets[i,]$Woeid <- strtoi(tweets[i,]$Woeid, base = 0L)}
+  
+  for(i in 1:nTweets){
+    tweets[i,]$Woeid <- strtoi(tweets[i,]$Woeid, base = 0L)
+  }
+  
   tweets <- tweets[!is.na(tweets$Woeid),]
   nTweets<-length(tweets$Tweets)
   myCorpus<-Corpus(VectorSource(tweets$Tweets))
+  
   #remove puntuation
   #myCorpus1<-tm_map(myCorpus,removePunctuation,ucp=TRUE)
   
   #remove capitals
   #for(i in 1:nTweets){myCorpus[[i]]$content<-tolower(myCorpus[[i]]$content)}
   myCorpus <- tm_map(myCorpus, tolower)
-  #function that remove accents
-  removeAccents <- content_transformer(function(x) chartr("ï¿½ï¿½ï¿½ï¿½ï¿½", "aeiou", x))
+  
+  #function that remove accents, file encode changes the str
+  removeAccents <- content_transformer(function(x) chartr("áéíóú", "aeiou", x))
   myCorpus <- tm_map(myCorpus, removeAccents)
+  
   #function that remove URLs
   removeURL<-function(x)gsub("http(s)?://[[:alnum:]]*(.[[:alnum:]]*)*(/[[:alnum:]]*)*","",x) 
   myCorpus<-tm_map(myCorpus,removeURL)        
   myCorpus<-tm_map(myCorpus,removePunctuation,ucp=TRUE)
+  
   #remove words without a lexical load
-  myStopwords<-stopwords('spanish')
-  myCorpus<-tm_map(myCorpus,removeWords,myStopwords)
+  #myStopwords<-stopwords('spanish')
+  #myCorpus<-tm_map(myCorpus,removeWords,myStopwords)
+  
   #eliminar caracteres raros
   removeCRT<-function(x)gsub("(<[[:alnum:]]*([[:punct:]][[:alnum:]]*)?>)*","",x)
   myCorpus<-tm_map(myCorpus,removeCRT) 
@@ -45,6 +55,13 @@ for (day in 19:28){
   m=data.frame(text = sapply(myCorpus, as.character), stringsAsFactors = FALSE)
   tweets <- tweets[,"Woeid"]
   tweets<-cbind(tweets, m)
-  name <- paste("datawarehouse/clean_data/clean_tweets_2019-04", name1, sep="-")
-  write.table(tweets, row.names = FALSE, file = name, sep = ";", fileEncoding = "UTF-8", append = FALSE)
+  
+  clean_corpus <- strsplit(file, "tweets_")
+  clean_corpus <- paste("clean_tweets", clean_corpus[[1]][2], sep="_")
+  clean_corpus <- paste("datawarehouse/clean_data", clean_corpus, sep="/")
+  
+  print(sprintf("Writing in: %s", clean_corpus))
+  write.table(tweets, row.names = FALSE, file = clean_corpus, sep = ";", fileEncoding = "UTF-8")
 }
+
+print("Script ended")
--- clean_data/countWords.R
+++ clean_data/countWords.R
@@ -2,7 +2,7 @@
 
 rm(list=ls())
 
-if (!is.installed("tm")){
+if (!"tm" %in% rownames(installed.packages())){
   install.packages("tm")
 }
 
@@ -11,41 +11,47 @@ require(tm)
 
 
 cities <- read.csv("datawarehouse/cities.csv", sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
+files <- list.files(path="datawarehouse/clean_data", pattern="*.csv", full.names=TRUE, recursive=FALSE)
+
 #load the words list
 words <- read.csv("datawarehouse/wordsList_v2.csv", sep = ";", fileEncoding = "windows-1252", header=TRUE)
-def <- data.frame(matrix(ncol = 6, nrow = 0))
-colnames(def) <- c("City", "Support_PP", "Support_PSOE", "Support_Cs", "Support_Podemos", "Support_VOX")
-for (day in 19:28){
-  name1 <- paste(day, "csv", sep=".")
-  name <- paste("datawarehouse/clean_data/clean_tweets_2019-04", name1, sep="-")
-  clean_tweets <- read.csv(name, sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
+
+out_path <- "datawarehouse/count_list"
+
+for (file in files){
+  
+  clean_tweets <- read.csv(file, sep = ";", fileEncoding = "UTF-8", header=TRUE, check.names=TRUE)
+  
   df <- data.frame(matrix(ncol = 6, nrow = 0))
   colnames(df) <- c("City", "Support_PP", "Support_PSOE", "Support_Cs", "Support_Podemos", "Support_VOX")
+  
   #count words for each city.
-  for(i in 1:10){
-    woeid <- cities[i,]$Woeid
-    citie <- cities[i,]$Name
+  for(i in 1:nrow(cities)) {
+    
+    woeid <- cities[i, ]$Woeid
+    city_name <- cities[i, ]$Name
+    
     #get the data of the same woeid
-    tweets_citie <- subset(clean_tweets, tweets==woeid)
+    tweets_city <- subset(clean_tweets, tweets==woeid)
+    
     #form a corpus with the previous data(the same woeid).
-    myCorpus<-Corpus(VectorSource(tweets_citie$text))
+    myCorpus <- Corpus(VectorSource(tweets_city$text))
+    
     #get the terms and their frequency
     terms <- TermDocumentMatrix(myCorpus)
     matrix_terms = as.matrix(terms)
     matrix_sort <- sort(rowSums(matrix_terms),decreasing=TRUE)
     frec_terms <- data.frame(word = names(matrix_sort), freq=matrix_sort)
+    
     dff <- data.frame(
-      City = citie,
+      City = city_name,
       Support_PP = 0,
       Support_PSOE = 0,
       Support_Cs = 0,
       Support_Podemos = 0,
       Support_VOX = 0
     )
-    #initialize definitive data frame
-    if(day == 19){
-      def <- rbind(def, dff)
-    }
+    
     #search for each word in the "wordList" in the dataframe of frec terms
     for (j in 1:nrow(words)){
       r <- match(words[j,1], frec_terms$word)
@@ -65,12 +71,13 @@ for (day in 19:28){
 
     df <- rbind(df, dff)
   }
-  #update the definitive dataframe with the data for each day
-  for(k in 2:6){
-    def[,k] = def[,k]+df[,k]
-  }
-
+  
+  # Let's save the day "X" results
+  day_results <- strsplit(file, "clean_tweets_")
+  day_results <- paste("count_list", day_results[[1]][2], sep="_")
+  day_results <- paste(out_path, day_results, sep="/")
+  print(sprintf("Writing in: %s", day_results))
+  write.table(df, row.names = FALSE, file = day_results, sep = ";", fileEncoding="UTF-8") 
 }
-write.table(def, row.names = FALSE, append = FALSE, file = "datawarehouse/count_list.csv", sep = ";",
-            fileEncoding="UTF-8")
 
+print("Script ended")

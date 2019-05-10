ab84ec76c7b7aef8207fbf8450b3e17e1a390e28
laudf <ladelafu@ucm.es>
2019-05-11 01:56:07 +0200

    fixed path in generate_basic_graph

--- graph_scripts/generate_basic_graph.py
+++ graph_scripts/generate_basic_graph.py
@@ -10,8 +10,7 @@ import networkx as nx
 import pandas as pd
 
 data_path = 'datawarehouse'
-print (os.getcwd())
-os.chdir("C:/Users/laufu/Documents/Cuarto/Segundo cuatri/MIN/Proyecto/ElectionsPredictor-Complex_Networks") 
+
 
 def normalize(x: int, values: list) -> int:
     if max(values) - min(values) == 0:
